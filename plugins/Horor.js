48c26952c6225056f946e7c8bb1520850a453363
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 03:50:55 +0700

    Update Horor.js

--- plugins/Horor.js
+++ plugins/Horor.js
@@ -4,16 +4,45 @@ let handler = async (m, { conn, command }) => {
   if (res.status != 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-await conn.sendButtonLoc(m.chat, await (await fetch(json.thumb)).buffer(), `
+  let str = `
 _*${json.judul}*_
 _${json.desc}_
-`.trim(), watermark, 'Ceritahororr', `.ceritahorror`)
+`.trim()
+const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+    templateMessage: {
+        hydratedTemplate: {
+          hydratedContentText: str,
+          locationMessage: { 
+          jpegThumbnail: json.thumb,           
+          hydratedFooterText: wm,
+          hydratedButtons: [{
+            urlButton: {
+              displayText: 'Official instagran',
+              url: 'https://instagram.com/ahmdlui'
+            }
+
+          },
+              {
+            quickReplyButton: {
+              displayText: 'Next',
+              id: '.ceritahoror',
+            }
+
+          }]
+        }
+      }
+    }), { userJid: m.sender, quoted: m });
+   //conn.reply(m.chat, text.trim(), m)
+   return await conn.relayMessage(
+        m.chat,
+        template.message,
+        { messageId: template.key.id }
+    )
 }
+
 handler.help = ['ceritahoror', 'ceritahrorr']
 handler.tags = ['internet', 'fun', 'quotes']
 handler.command = /^ceritahoror|ceritahorror$/i
 
-handler.limit = true
 
 module.exports = handler
-
