31ff9b9c138db86def541a70346e981e59b06b71
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 04:15:13 +0700

    Update Horor.js

--- plugins/Horor.js
+++ plugins/Horor.js
@@ -4,42 +4,11 @@ let handler = async (m, { conn, command }) => {
   if (res.status != 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  let str = `
-_*${json.judul}*_
-_${json.desc}_
-`.trim()
-const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-    templateMessage: {
-        hydratedTemplate: {
-          hydratedContentText: str,
-          locationMessage: { 
-          jpegThumbnail: json.thumb,           
-          hydratedFooterText: wm,
-          hydratedButtons: [{
-            urlButton: {
-              displayText: 'Official instagran',
-              url: 'https://instagram.com/ahmdlui'
-            }
-
-          },
-              {
-            quickReplyButton: {
-              displayText: 'Next',
-              id: '.ceritahoror',
-            }
-
-          }]
-        }
-      }
-    }), { userJid: m.sender, quoted: m });
-   //conn.reply(m.chat, text.trim(), m)
-   return await conn.relayMessage(
-        m.chat,
-        template.message,
-        { messageId: template.key.id }
-    )
+let { judul, desc, thumb } = json.results[0]
+let demit = `*Judul:* ${judul}
+*Deskripsi:* ${desc}`
+  conn.sendFile(m.chat, thumb, '', demit, m)
 }
-
 handler.help = ['ceritahoror', 'ceritahrorr']
 handler.tags = ['internet', 'fun', 'quotes']
 handler.command = /^ceritahoror|ceritahorror$/i
