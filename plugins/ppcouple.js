4d9263e413704a78214862a09e6bf11a1c960927
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 03:15:34 +0700

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -1,20 +1,29 @@
-let fetch = require("node-fetch")
-
-let handler = async (m, { conn }) => {
-//https://api.lolhuman.xyz/api/random/ppcouple?apikey=RFK-Rey
-m.reply(wait)
-
-let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
-let json = await res.json()
-
-conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
-
-conn.sendFile(m.chat, json.result.female, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
-
+let { Presence } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let limit = 1
+let handler  = async (m, { conn, args, usedPrefix, command }) => {
+	await conn.updatePresence(m.chat, Presence.composing) 
+	let text = args.join` `
+	fetch('https://leyscoders-api.herokuapp.com/api/ppcouple?apikey=MIMINGANZ')
+    	.then(res => res.json())
+    	.then(json => {
+    		conn.updatePresence(m.chat, Presence.composing) 
+    		conn.reply(m.chat, `\`\`\`Tunggu sebentar . . .\`\`\``, m)
+			conn.sendFile(m.chat, json.result.male, 'male.jpg', 'ini cowoknya', m)	
+			conn.sendFile(m.chat, json.result.female, 'female.jpg', 'nih ceweknya',m)
+	}) .catch(() => { conn.reply(m.chat, `*Terjadi kesalahan . . .*`, m) })
 }
-handler.help = ['ppcouple', 'ppcp']
-handler.tags = ['internet', 'anime']
-handler.command = /^(pp(cp|couple))$/i
+handler.help = ['ppcouple']
+handler.tags = ['anime']
+handler.command = /^(ppcouple)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
 handler.limit = true
-
+handler.exp = 15
 module.exports = handler
