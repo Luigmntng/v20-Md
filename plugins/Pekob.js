cc964aa7489a1e7aa6a1181b6e51f9434dac939d
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 04:03:55 +0700

    Update Pekob.js

--- plugins/Pekob.js
+++ plugins/Pekob.js
@@ -1,16 +1,13 @@
- fetch = require('node-fetch')
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let res = await fetch(API('https://hmtai.herokuapp.com', '/nsfw/' + command, {}))
 	if (!res.ok) throw `${res.status} ${res.statusText}`
 	let json = await res.json()
-	if (json.url) {
-		let buffer = await (await fetch(json.url)).buffer()
-		await conn.sendFile(m.chat, url, 'json.url', `Sagne sama kartun ?`.trim(), m)
+	if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'sgne sama kartun ?', m, 0, { thumbnail: Buffer.alloc(0) })
 }
-
 handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
-handler.tags = ['dewasa']
+handler.tags = ['nsfw']
 handler.command = /^(ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
 module.exports = handler
-
