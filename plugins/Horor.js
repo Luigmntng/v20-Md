8357aacbdc936311c98e0e784f15ed74ec6e302a
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 16:02:12 +0700

    Update Horor.js

--- plugins/Horor.js
+++ plugins/Horor.js
@@ -4,12 +4,9 @@ let handler = async (m, { conn, command }) => {
   if (res.status != 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  let caption = `
-_*${json.judul}*_
-_${json.desc}_
-`.trim()
-await conn.sendFile(m.chat, await (await fetch(json.thumb)).buffer(), caption, m)
-}
+await conn.sendFile(m.chat, await (await fetch(json.thumb)).buffer(), '', '_*${json.judul}*_\n\n_${json.desc}_', m)
+
+ }
 handler.help = ['ceritahoror', 'ceritahrorr']
 handler.tags = ['internet', 'fun', 'quotes']
 handler.command = /^ceritahoror|ceritahorror$/i
