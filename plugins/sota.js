36658184288cf5c2ed771466ecae9db28ce9e3f1
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-04 13:06:29 +0700

    Update sota.js

--- plugins/sota.js
+++ plugins/sota.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, usedPrefix }) => {
 	
-	conn.sendButtonImg(m.chat, await ( await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/shota?apikey=apirey`)).buffer(), 'Udah kak Jgn Lupa Follow ig @ahmdlui', footer, 'Adek gw Banhg', `.souta`, m)
+	conn.sendFile(m.chat, await ( await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/shota?apikey=apirey`)).buffer(), '', 'Udah kak Jgn Lupa Follow ig @ahmdlui', m)
 
 }
 
 handler.help = ['souta']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(souta)$/i
 handler.register = true
 
