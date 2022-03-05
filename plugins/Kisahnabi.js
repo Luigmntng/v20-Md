dc2cf11323a9b8b942bfcb734aee7c9a15eb639f
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-05 18:12:33 +0700

    Update Kisahnabi.js

--- plugins/Kisahnabi.js
+++ plugins/Kisahnabi.js
@@ -1,35 +1,18 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, 'Masukan Nama Nabi nya', m)
-  await m.reply(global.wait)
-  let res = await fetch(`https://restapi-production-a62b.up.railway.app/api/muslim/kisahnabi?nabi=${text}&apikey=APIKEY`)
-  if (res.status != 200) throw await res.text()
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `Masukkan query!`
+  let res = await fetch(https://restapi-production-a62b.up.railway.app/api/muslim/kisahnabi?nabi=${text}&apikey=APIKEY)
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.status) throw json
-  let caption = `
-*「 Kisah Nabi 」*
-*Nama Nabi* : ${name}
-*Kelahiran* : ${kelahiran}
-*Usia* : ${wafat_usia}
-*Singgah* : ${singgah}
-Kisah : _${kisah}_
-`.trim()
-  await m.reply(caption)
+  let { name, kelahiran, wafat_usia, singgah, kisah, } = json.results[0]
+let kisahnabi = `*NAMA NABI:* ${name}
+*KELAHIRAN:* ${kelahiran}
+*USIA:* ${wafat_usia}
+*AL KISAH:*  ${kisah}`
+  conn.reply(m.chat, kisahnabi, m)
 }
-handler.help = ['kisah|kisahnabi|nabi'].map(v => v + ' <nama nabi>')
+handler.help = ['kisahnabi <namanabi>']
 handler.tags = ['quran']
-handler.command = /^(kisah|kisahnabi|nabi)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
+handler.command = /^(kisahnabi)$/i
+//udah di maapin kan?
 module.exports = handler
