d85be8221aaec9666710ad46909e5aebb78e8f15
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-08 03:00:12 +0700

    Update waibu.js

--- plugins/waibu.js
+++ plugins/waibu.js
@@ -5,9 +5,9 @@ m.reply('_Loading..._')
 
 }
 
-handler.help = ['chitoge', 'kaori', 'nezuko', 'tsunade', 'emilia', 'asuna', 'sagiri', 'miku', 'yotsuba', 'mikasa', 'shina', 'gremory', 'akira', 'toukachan' 'kagura', 'hinata', 'minato', 'naruto', 'onepiece', 'itori', 'sasuke', 'kurumi', 'anna', 'deidara', 'shinomiya', 'eba', 'yumeko', 'akiyama', 'yuki', 'itachi', 'madara', 'hestia', 'inori', 'sakura', 'keneki', 'shinka', 'chiho', 'kaga', 'rize', 'erza', 'ayuzawa', 'kotori', 'boruto']
+handler.help = ['chitoge', 'kaori', 'nezuko', 'tsunade', 'emilia', 'asuna', 'sagiri', 'miku', 'yotsuba', 'mikasa', 'shina', 'gremory', 'akira', 'toukachan' 'kagura', 'hinata', 'minato', 'naruto', 'onepiece', 'itori', 'sasuke', 'kurumi', 'anna', 'deidara', 'shinomiya', 'eba', 'yumeko', 'akiyama', 'yuki', 'itachi']
 handler.tags = ['anime']
-handler.command = /^(chitoge|kaori|nezuko|tsunade|emilia|asuna|sagiri|miku|yotsuba|mikasa|shina|gremory|akira|toukachan|kagura|hinata|minato|naruto|onepiece|itori|sasuke|kurumi|anna|deidara|shinomiya|eba|yumeko|akiyama|yuki|itachi|madara|hestia|inori|sakura|keneki|shinka|chiho|kaga|rize|erza|ayuzawa|kotori|boruto)$/i
+handler.command = /^(chitoge|kaori|nezuko|tsunade|emilia|asuna|sagiri|miku|yotsuba|mikasa|shina|gremory|akira|toukachan|kagura|hinata|minato|naruto|onepiece|itori|sasuke|kurumi|anna|deidara|shinomiya|eba|yumeko|akiyama|yuki|itachi)$/i
 handler.register = true
 
 handler.limit = true
