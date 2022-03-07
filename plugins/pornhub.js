40bd93b02abe887305d3b56d32b072b8ecf07f0a
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-08 02:45:23 +0700

    Update pornhub.js

--- plugins/pornhub.js
+++ plugins/pornhub.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
     let res = await fetch(global.API('lui', '/api/downloader/pornhub', { query: text }, 'apikey'))
     if (!res.ok) throw await res.text()
     let json = await res.json()
-    let keqing = json.results.map((v, i) => `#${i + 1}. \n*Judul:* ${v.title}\n*Dilihat:* ${v.views}\n*Pembuat:* ${v.author}\n*Durasi:* ${v.duration}*HD:* {v.hd}\n*PREMIUM:* ${v.premium}\n*Link:* ${v.link}}\n==============\n`).join('\n') 
+    let keqing = json.result.map((v, i) => `#${i + 1}. \n*Judul:* ${v.title}\n*Dilihat:* ${v.views}\n*Pembuat:* ${v.author}\n*Durasi:* ${v.duration}\n*Link:* ${v.link}\n*HD:* ${v.hd}\n*Premium:* {v.premium}}\n==============\n`).join('\n') 
     if (json.status) m.reply(keqing)
     else throw json
 }
