cdec40667db11712907f9a5b1b07b01127c536be
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-08 02:52:06 +0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,16 +1,15 @@
-
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-    let res = await fetch(global.API('lui', '/api/downloader/xnxx', { query: text }, 'apikey'))
+    let res = await fetch(global.API('lui', '/api/downloader/pornhub', { query: text }, 'apikey'))
     if (!res.ok) throw await res.text()
     let json = await res.json()
-    let keqing = json.result.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}}\n==============\n`).join('\n') 
+    let keqing = json.results.map((v, i) => `#${i + 1}. \n*Judul:* ${v.title}\n*Dilihat:* ${v.views}\n*Pembuat:* ${v.author}\n*Durasi:* ${v.duration}\n*Link:* ${v.link}\n*HD:* ${v.hd}\n*Premium:* {v.premium}}\n==============\n`).join('\n') 
     if (json.status) m.reply(keqing)
     else throw json
 }
-handler.help = ['xnxxsearch <query>']
-handler.tags = ['internet']
-handler.command = /^(xnxxsearch)$/i
+handler.help = ['tester <judul>']
+handler.tags = ['bokep']
+handler.command = /^(tester)$/i
 
 handler.limit = true
 
