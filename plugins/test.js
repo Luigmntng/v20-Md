1ea8a2877860422c81f441f44819a222dcb95c5a
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-07 16:10:34 +0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,17 +1,16 @@
+
 let fetch = require('node-fetch')
-let handler = async(m, { conn, args }) => {
-    response = args.join (' ')
-  if (!args[0]) throw 'Masukkan Judul!'
-  m.reply('follow ig @ahmdlui')
-    let res = `https://restapi-production-a62b.up.railway.app/api/downloader/xnxx?query=${response}&apikey=APIKEY`
+let handler = async(m, { conn, text }) => {
+    let res = await fetch(global.API('lui', '/api/downloader/xnxx', { query: text }, 'apikey'))
+    if (!res.ok) throw await res.text()
     let json = await res.json()
     let keqing = json.result.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}}\n==============\n`).join('\n') 
     if (json.status) m.reply(keqing)
     else throw json
 }
-handler.help = ['xxsearch <query>']
+handler.help = ['xnxxsearch <query>']
 handler.tags = ['internet']
-handler.command = /^(xxsearch)$/i
+handler.command = /^(xnxxsearch)$/i
 
 handler.limit = true
 
