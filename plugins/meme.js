262531e04699c756d648dc32275b56cb424fc3e9
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-08 18:35:18 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,17 +1,24 @@
-let fetch = require("node-fetch")
-let handler = async (m, { conn }) => {
-  m.reply(wait)
-  let res = await fetch(global.API('https://some-random-api.ml', '/meme'))
-  if (!res.ok) throw eror
-  let json = await res.json()
-  if (!json.image) throw 'Err!'
-  let thumbnail = await (await fetch(json.image)).buffer()
-  conn.sendFile(m.chat, json.image, 'meme.png', json.caption, m, 0, { thumbnail })
+//created by Scooppt
+let fetch = require('node-fetch')
+
+let handler  = async (m, { conn, text }) => {
+  try {
+     let res = await fetch('https://hardianto.xyz/api/darkmeme?apikey=hardianto')
+     let img = await res.buffer()
+     let caption = `
+Ini meme?
+`.trim()
+    conn.sendFile(m.chat, img, 'waifu.jpg', caption, m)
+   } catch (e) {
+        console.log(e)
+        throw '_*Erro!*_'
+    }
 }
 
 handler.help = ['meme']
 handler.tags = ['internet']
+handler.command = /^meme$/i
 
-handler.command = /^(meme)$/i
+handler.fail = null
 
 module.exports = handler
