39ccb06289cb3ff2698c9af50788db34774a9dd6
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-08 18:51:30 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,24 +1,14 @@
-const { igdl } = require('../lib/scrape')
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  if (!args[0]) throw `Harap masukkan URL Instagram yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
-
-  igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
-    await m.reply(global.wait)
-    for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), watermark, m)
-    }
-  })
-
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async(m, { conn, usedPrefix, text, command }) => {
+    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://www.instagram.com/reel/CaivjKDsNhe/?utm_medium=copy_link`
+    let res = await fetch(global.API('hari', '/api/download/instadl', { url: text }, 'apikey'))
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.medias.url, '', 'Jangan Lupa Follow Ig saya @ahmdlui', m)
 }
-handler.help = ['ig'].map(v => v + ' <url>')
+handler.help = ['ig <link>']
 handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
-
-handler.limit = true
-
+handler.command = /^ig$/i
+handler.limit = 15
 module.exports = handler
