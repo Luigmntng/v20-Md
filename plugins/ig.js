2fc47c911b4340cf7f13331c954db690044a1a27
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-09 17:46:40 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
-    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://www.instagram.com/reel/CaivjKDsNhe/?utm_medium=copy_link`
+    if (!text) throw `Harap masukkan URL sebagai konten\n\nContoh: ${usedPrefix + command} https://www.instagram.com/reel/CaivjKDsNhe/?utm_medium=copy_link`
     let res = await fetch(global.API('hari', '/api/download/instadl', { url: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.medias.url, '', 'Jangan Lupa Follow Ig saya @ahmdlui', m)
+    await conn.sendFile(m.chat, json.url, '', 'Jangan Lupa Follow Ig saya @ahmdlui', m)
 }
 handler.help = ['ig <link>']
 handler.tags = ['downloader']
