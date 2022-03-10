c6a4520edcf236243377655f04166143a69d118f
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-11 00:09:48 +0700

    Update cinta.js

--- plugins/cinta.js
+++ plugins/cinta.js
@@ -6,8 +6,8 @@ let res = await axios.get(API('http://docs-jojo.herokuapp.com', '/api/katacinta'
 let json = res.data
     m.reply(m.chat, json.result, m)
 }
-handler.help = ['husbu2']
-handler.tags = ['anime']
-handler.command = /^(husbu2)$/i
+handler.help = ['katacinta']
+handler.tags = ['quotes']
+handler.command = /^(katacinta)$/i
 
 module.exports = handler
