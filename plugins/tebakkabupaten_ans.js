edb402d07aa7c3a3b418a0dfd21bbd70bea784e4
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-10 23:42:29 +0700

    Update tebakkabupaten_ans.js

--- plugins/tebakkabupaten_ans.js
+++ plugins/tebakkabupaten_ans.js
@@ -3,12 +3,11 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tekb/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ineedhelp/i.test(m.quoted.contentText)) return !0
     this.tebakkabupaten = this.tebakkabupaten ? this.tebakkabupaten : {}
     if (!(id in this.tebakkabupaten)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.tebakkabupaten[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebakkabupaten[id][1]))
-        if (['.tekb', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.title.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakkabupaten[id][2]
             m.reply(m.chat, `*Benar!* +${this.tebakkabupaten[id][2]} XP`, m)
