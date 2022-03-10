97d5f6ae62fae37a2b84c57469baf2148a946d40
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-10 18:30:24 +0700

    Update wanted.js

--- plugins/wanted.js
+++ plugins/wanted.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, text }) => {
    conn.sendFile(m.chat, wanted, '', 'Yoi work g sih ?', m)
    
  } catch (e) {
-   m.reply('Conversion Failed')
+   m.reply('Reply Fotonya! atau Eror dalam membuat :v')
   }
 }
 handler.help = ['wanted']
