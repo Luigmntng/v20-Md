f7473a22dee2cc65203f665ee8f7ff0a28f0f1ac
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-10 23:36:03 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,13 +1,12 @@
 let fetch = require('node-fetch')
 let fs = require('fs')
-let handler = async(m, { conn, usedPrefix, text, command }) => {
-    m.reply('tunggu sebentar...')
+let handler = async(m, { conn, usedPrefix, command }) => {
     let res = await fetch(global.API('http://docs-jojo.herokuapp.com', '/api/husbuando')
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     await conn.sendFile(m.chat, json.image '', 'Jangan Lupa Follow Ig saya @ahmdlui', m)
 }
-handler.help = ['husbu']
+handler.help = ['husbu]
 handler.tags = ['anime']
 handler.command = /^husbu$/i
 handler.limit = true
