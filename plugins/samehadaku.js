39e37e1a6b8218eb936b78afdc12ae7f5ef60e17
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-10 23:00:23 +0700

    Create samehadaku.js

---
+++ plugins/samehadaku.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+let handler = async(m, { conn, text, usedPrefix }) => {
+
+   if (!text) throw `Masukkan query!`
+    await m.reply(global.wait)
+  let res = await fetch(global.API('http://docs-jojo.herokuapp.com', '/api/samehadaku', { q: text }))
+  i .then((res) => {
+          let hasil = `*Judul:* ${res.data.result.title}\n\n*Link:* ${res.data.link}\n*Deskripsi:* ${res.data.desc}`
+            conn.reply(m.chat, json.thumb, 'same.jpg', hasil, m)
+        })
+        .catch()
+}
+handler.help = ['samehadaku <nama anime nya>']
+handler.tags = ['anime']
+handler.command = /^(samehadaku)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+
+module.exports = handler
+
+
