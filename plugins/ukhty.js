93093fbb348d10951fd44eb87801aac0089ee4e3
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-15 22:38:17 +0700

    Update ukhty.js

--- plugins/ukhty.js
+++ plugins/ukhty.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.result.url) throw 'Error!'
-  conn.sendFile(m.chat, json.result.url, '', 'Istri kartun', m)
+  conn.sendFile(m.chat, json.result.url, '', 'Follow ig sya kak @ahmdlui', m)
 }
 handler.help = ['ukhty']
 handler.tags = ['asupan']
